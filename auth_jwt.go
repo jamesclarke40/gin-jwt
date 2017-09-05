01699b61c1815e4e1b7fc5b575619349920a5140
James Clarke <jamesclarke40@gmail.com>
2017-09-05 17:01:58 +0100

    Tweaked Login Handler for personal use

--- auth_jwt.go
+++ auth_jwt.go
@@ -38,7 +38,7 @@ type GinJWTMiddleware struct {
 	// Callback function that should perform the authentication of the user based on userID and
 	// password. Must return true on success, false on failure. Required.
 	// Option return user id, if so, user id will be stored in Claim Array.
-	Authenticator func(userID string, password string, c *gin.Context) (gin.H, bool)
+	Authenticator func(username string, password string, c *gin.Context) (gin.H, bool)
 
 	// Callback function that should perform the authorization of the authenticated user. Called
 	// only after an authentication success. Must return true on success, false on failure.
@@ -349,7 +349,7 @@ func ExtractClaims(c *gin.Context) jwt.MapClaims {
 }
 
 // TokenGenerator handler that clients can use to get a jwt token.
-func (mw *GinJWTMiddleware) TokenGenerator(userID string) string {
+func (mw *GinJWTMiddleware) TokenGenerator(userID int64) string {
 	token := jwt.New(jwt.GetSigningMethod(mw.SigningAlgorithm))
 	claims := token.Claims.(jwt.MapClaims)
 
