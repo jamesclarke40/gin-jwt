82c8eb0a6eb8b99578e0e63532c5415cd96fac5c
James Clarke <jamesclarke40@gmail.com>
2017-09-05 09:18:27 +0100

    Tweaked Login Handler for personal use

--- auth_jwt.go
+++ auth_jwt.go
@@ -262,12 +262,14 @@ func (mw *GinJWTMiddleware) RegHandler(c *gin.Context) {
 		mw.unauthorized(c, http.StatusInternalServerError, "User already exists")
 		return
 	}
+
 	if mw.registrator == nil {
 		mw.unauthorized(c, http.StatusInternalServerError, "Missing registrator func")
 		return
 	}
 
-	dets, _ := mv.registrator(regVals.Firstname, regVals.Lastname, regVals.Username, regVals.Password, c)
+	dets = gin.H{}
+	//dets, _ := mv.registrator(regVals.Firstname, regVals.Lastname, regVals.Username, regVals.Password, c)
 
 	// Create the token
 	token := jwt.New(jwt.GetSigningMethod(mw.SigningAlgorithm))
