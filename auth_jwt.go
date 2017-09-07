96749ee47d67789452830b5215600e1870deba36
James Clarke <jamesclarke40@gmail.com>
2017-09-07 15:26:26 +0100

    Tweaked Login Handler for personal use

--- auth_jwt.go
+++ auth_jwt.go
@@ -132,7 +132,7 @@ func (mw *GinJWTMiddleware) MiddlewareInit() error {
 
 	if mw.IdentityHandler == nil {
 		mw.IdentityHandler = func(claims jwt.MapClaims) int64 {
-			fmt.Sprintf("%T", claims["id"])
+			fmt.Sprintf("TYPE: %T", claims["id"])
 			return claims["id"].(int64)
 		}
 	}
